d1287e8fc874eff402458f604ed07a7e4fda7419
Gareth Solbeck <gsolbeck@gmail.com>
2015-10-11 12:47:46 -0400

    Fix off-by-one errors in grid conversion functions

--- src/ldutil.cpp
+++ src/ldutil.cpp
@@ -11,8 +11,8 @@
 // Convert grid position to world position
 df::Position gridToPos( df::Position pos ) {
   // Bound input values to grid
-  int temp_x = std::max(0, std::min(GRID_WIDTH, pos.getX()));
-  int temp_y = std::max(0, std::min(GRID_HEIGHT, pos.getY()));
+  int temp_x = std::max(0, std::min(GRID_WIDTH - 1, pos.getX()));
+  int temp_y = std::max(0, std::min(GRID_HEIGHT - 1, pos.getY()));
   temp_x = MARGIN_LEFT + temp_x * GRID_SIZE + GRID_SIZE / 2;
   temp_y = MARGIN_TOP + temp_y * GRID_SIZE + GRID_SIZE / 2;
   return df::Position(temp_x, temp_y);
@@ -25,8 +25,8 @@ df::Position posToGrid( df::Position pos ) {
   int temp_x = (pos.getX() - MARGIN_LEFT) / GRID_SIZE;
   int temp_y = (pos.getY() - MARGIN_TOP) / GRID_SIZE;
   // Bound output values to grid
-  temp_x = std::max(0, std::min(GRID_WIDTH, temp_x));
-  temp_y = std::max(0, std::min(GRID_HEIGHT, temp_y));
+  temp_x = std::max(0, std::min(GRID_WIDTH - 1, temp_x));
+  temp_y = std::max(0, std::min(GRID_HEIGHT - 1, temp_y));
   return df::Position(temp_x, temp_y);
 }
 
